3bdbc88e19eeecb2e8c66399a951e8928a185a70
Andrey Antonov <andsmet@yandex.ru>
2023-10-12 14:19:10 +0300

    fixed classes Site and Page. name class Enum -> Status

--- src/main/java/searchengine/model/Page.java
+++ src/main/java/searchengine/model/Page.java
@@ -1,19 +1,29 @@
 package searchengine.model;
 
-import org.springframework.data.annotation.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
-@Entity
-@Table(name = "page")
+@Entity(name = "page")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Page {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(name = "site_id")
+
+    @Column(name = "site_id", columnDefinition = "INT")
     private int siteId;
 
+    @Column(columnDefinition = "TEXT")
     private String path;
+
+    @Column(columnDefinition = "INT")
     private int code;
+
+    @Column(columnDefinition = "MEDIUMTEXT")
     private String content;
 }
--- src/main/java/searchengine/model/Site.java
+++ src/main/java/searchengine/model/Site.java
@@ -1,81 +1,35 @@
 package searchengine.model;
 
-import org.springframework.data.annotation.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 
-@Entity
-@Table(name = "site")
+@Entity(name = "site")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Site {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    //@Enumerated(EnumType.STRING)
-    @Column(columnDefinition = "Enum('INDEXING', 'INDEXED', 'FAILED'")
-    //@Column(columnDefinition = "Enum")
-    private String status;
+    @Enumerated(EnumType.STRING)
+    private Status status;
 
-    @Column(name = "Status_time")
+    @Column(name = "Status_time", columnDefinition = "DATETIME")
     private Date statusTime;
 
-    @Column(name = "last_error")
+    @Column(name = "last_error", nullable = false, columnDefinition = "TEXT")
     private String error;
 
-    @Column(name = "url")
+    @Column(name = "url", columnDefinition = "VARCHAR(255)")
     private String urlSite;
 
-    @Column(name = "name")
+    @Column(name = "name", columnDefinition = "VARCHAR(255)")
     private String nameSite;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Date getStatusTime() {
-        return statusTime;
-    }
-
-    public void setStatusTime(Date statusTime) {
-        this.statusTime = statusTime;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
-    }
-
-    public String getUrl() {
-        return urlSite;
-    }
-
-    public void setUrl(String url) {
-        this.urlSite = url;
-    }
-
-    public String getName() {
-        return nameSite;
-    }
-
-    public void setName(String name) {
-        this.nameSite = name;
-    }
 }
